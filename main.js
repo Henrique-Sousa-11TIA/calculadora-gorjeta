e02a0b1e5feb59078008fdc259796282355ee937
henry.sousa.2022 <henry.sousa.2022@gmail.com>
2025-04-01 22:45:47 +0000

    criação da lógica de selecionar o botao de gorjeta ou colocar o valor customizado

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
-let bill = 0;
-let tipPercentage = 0;
-let numberOfPeople = 0;
-let buttonSelected = null;  
-
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue() {
-    bill = document.querySelector("#bill").valueAsNumber;
+    bill = document.querySelector("#bill").valueAsNumber
+    removeClassButtonSelected()
 }
-
 
 function receiveNumberOfPeopleValue() {
-    numberOfPeople = document.querySelector("#people").valueAsNumber;
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+    removeClassButtonSelected()
 }
 
-
 function receiveTipPercentageValue(value) {
-    tipPercentage = value / 100;
+    tipPercentage = value / 100
 
-   
-    if (buttonSelected !== null) {
-        buttonSelected.classList.remove("button-selected");
-        buttonSelected.style.backgroundColor = "";  
+    if(document.querySelector("#custom-tip").value !== ""){
+        document.querySelector("#custom-tip").value = ""
     }
 
-    
-    buttonSelected = document.querySelector(`#button-${value}`);
-    buttonSelected.classList.add("button-selected");
-    buttonSelected.style.backgroundColor = "var(--topaz)"; 
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected.style.backgroundColor = ""
+    }
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+    buttonSelected.style.backgroundColor = "var(--topaz)"
+}
+
+function receiveCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+    removeClassButtonSelected()
+}
+
+function removeClassButtonSelected() {
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected.style.backgroundColor = ""
+        buttonSelected = null
+    }
 }
